ed59a21f972c79666731079060b7257d57d071d3
k4rolPr3ciado <k4rolpr3ci4do@gmail.com>
2026-04-21 09:48:21 -0600

    Primer Avance

---
+++ Funciones.js
@@ -0,0 +1,33 @@
+// Esperamos a que el DOM (el HTML) esté completamente cargado
+document.addEventListener("DOMContentLoaded", () => {
+
+    // Seleccionamos el botón por su ID
+    const botonConvocatorias = document.getElementById("btn-explorar");
+
+    // Verificamos que el botón exista en la página actual para evitar errores
+    if (botonConvocatorias) {
+        botonConvocatorias.addEventListener("click", () => {
+            // Redirigimos al archivo de convocatorias
+            window.location.href = "ExplorarConvoca.html";
+        });
+    }
+
+    const botonsolicitud = document.getElementById("btn-explorar1");
+
+
+    if (botonsolicitud) {
+        botonsolicitud.addEventListener("click", () => {
+            // Redirigimos al archivo de convocatorias
+            window.location.href = "Solicitud.html";
+        });
+    }
+
+    const botoncontrol = document.getElementById("btn-explorar2");
+
+    if (botoncontrol) {
+        botoncontrol.addEventListener("click", () => {
+            window.location.href = "ControlSeguimiento.html";
+        });
+    }
+    const 
+});
